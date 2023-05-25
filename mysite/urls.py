e11a93c922c1928fd7616ee548a17ef92f01655d
ali <ali@gmail.com>
2023-05-25 11:25:56 +0330

    added templates folder and html files to the app 'website' and render to views

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('website/', include('website.urls'))
 
 ]
